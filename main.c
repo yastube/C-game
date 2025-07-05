dce44df0cd4050d8751d5be502578efa61c68897
puffythedev <nicknameopg1@gmail.com>
2025-07-05 13:48:58 +0100

    using render texture for less for loops

--- main.c
+++ main.c
@@ -10,8 +10,9 @@ int main(void)
 {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(initialWidth, initialHeight, "Game");
+    RenderTexture2D texture = LoadRenderTexture(initialWidth, initialHeight);
     // SetTargetFPS(75);
-    Vector2 center = {200, 200};
+    Vector2 center = {GetRenderWidth() / 2, GetRenderHeight() / 2};
     float radius = 90;
     int segments = 90;
     float test[segments];
@@ -28,6 +29,8 @@ int main(void)
         ac += dt;
         if (ac >= 1 / 75.f)
         {
+            BeginTextureMode(texture);
+            ClearBackground(BLANK);
             ac = 0;
             for (int i = 0; i < segments; i++)
             {
@@ -36,22 +39,17 @@ int main(void)
                 test[i] = (sinf((r[i] / 9) + i) * 0.5f * 5);
                 Vector2 newPoint = {
                     center.x + (radius + test[i]) * cosf(angle),
-                    center.y + (radius + test[i]) * sinf(angle)};
+                    center.y + (radius + test[i]) * sinf(angle)
+                };
+                DrawLineV(prevpoint, newPoint, WHITE);
                 prevpoint = newPoint;
+    
             }
+            EndTextureMode();
         }
         BeginDrawing();
         ClearBackground(BLACK);
-        for (int i = 0; i < segments; i++)
-        {
-            float angle = 2 * PI * i / segments;
-            Vector2 newPoint = {
-                center.x + (radius + test[i]) * cosf(angle),
-                center.y + (radius + test[i]) * sinf(angle)
-            };
-            DrawLineV(prevpoint, newPoint, WHITE);
-            prevpoint = newPoint;
-        }
+        DrawTexture(texture.texture, 0, 0, WHITE);
         DrawText("GAME", 100, 50, 40, WHITE);
         DrawFPS(20, 20);
         EndDrawing();
